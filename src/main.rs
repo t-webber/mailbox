497bb71aed587eb941f211f06b0ede84f5ab1c67
Tom Webber <t.c.w.webber@gmail.com>
2025-04-07 07:55:02 +0200

    add: loading credentials from `.env`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env::var;
+
+use dotenv::dotenv;
+
+#[derive(Debug)]
+struct Credentials {
+    domain: String,
+    email: String,
+    password: String,
+}
+
+impl Credentials {
+    fn load() -> Option<Self> {
+        dotenv().ok()?;
+        Some(Self {
+            domain: var("DOMAIN").ok()?,
+            email: var("EMAIL").ok()?,
+            password: var("PASSWORD").ok()?,
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    dbg!(Credentials::load());
 }
